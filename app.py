873a1ce53b37a0641ed8db413498b06fba639b32
Daniil Timachov <danyatimachov@gmail.com>
2021-02-15 23:15:31 +0200

    Fetching and displaying the board from the link.

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
+from src.board_parser import get_pieces_map
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    url = 'https://www.chess.com/puzzles/problem/41839'
+    rendered_html = render_template(
+        'index.html', pieces_map=get_pieces_map(url))
+    with open('tmp/index.html', 'w') as f:
+        f.write(rendered_html)
+
+    return rendered_html
+
 
 if __name__ == '__main__':
     app.run(debug=True)
