7df99ffcd1522e91e390a36b24d23abac2086328
Daniil Timachov <danyatimachov@gmail.com>
2021-03-09 22:24:53 +0200

    🐛 FIX: Unnecessary checks.

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -9,9 +9,6 @@ onButtonDoNotClickClicked = () => {
 }
 
 function main() {
-    login.setCustomValidity("Login should contain only alphanumeric characters, \"_\" and \".\"");
-    password.setCustomValidity("Password should be at least 8 characters, contain 1 number and 1 uppercase letter");
-
     button.addEventListener('click', onButtonDoNotClickClicked);
 }
 
